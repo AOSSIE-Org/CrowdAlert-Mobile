e5ff448510bcdf75b6ec9f90cea1798a74fa3904
Vibhav Agarwal <vibhavagarwal5@gmail.com>
2018-05-20 14:00:44 +0530

    Styling changed

--- src/components/login.js
+++ src/components/login.js
@@ -17,13 +17,12 @@ import {bindActionCreators} from 'redux';
 import {connect} from "react-redux";
 import {fbSignIn , googleSignin} from '../actions/loginAction'
 import { Actions } from 'react-native-router-flux';
-import { GoogleSignin } from 'react-native-google-signin';
-import {styles} from '../assets/login_styles';
+import {styles} from '../assets/styles/login_styles';
 
 /**
  * This class defines UI of the main screen containing all login options.
  * The styling used has been imported from ../assets/login_styles.js
- * 
+ *
  * @class Login
  * @extends {Component}
  */
@@ -38,37 +37,37 @@ class Login extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <ActivityIndicator animating={this.props.login.loading} size={'large'}/>
-                <Image
-                    source={require('../assets/earthquake-30-512.png')}
-                    style={[styles.logo]}/>
+                {/* <ActivityIndicator animating={this.props.login.loading} size={'large'}/> */}
+                <View style={styles.headerImage}>
+                    <Image source={require('../assets/earthquake-30-512.png')} style={styles.logo}/>
+                </View>
                 <Text style={styles.welcome}>
                     Crowd Alert
                 </Text>
                 <View style={styles.button_container}>
-                <TouchableOpacity
-                    style={styles.button_1}
-                    onPress={() => Actions.signin()}>
-                    <Text style = {styles.button_text_signin}> SignIn </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.button_2}
-                    onPress={() => Actions.signup()}>
-                    <Text style = {styles.button_text_register}> Register </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button_1}
+                        onPress={() => Actions.signin()}>
+                        <Text style = {styles.button_text}> SignIn </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button_2}
+                        onPress={() => Actions.signup()}>
+                        <Text style = {styles.button_text}> Register </Text>
+                    </TouchableOpacity>
                 </View>
-                <View><Text style = {styles.Text}>More login options :</Text></View>
-                <View style={styles.button_container_social}>
-                <TouchableOpacity
-                    style={styles.button_fb}
-                    onPress={() => this.props.fbSignIn()}>
-                    <Text style = {styles.button_text_facebook}> Facebook </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.button_google}
-                    onPress={() => this.props.googleSignin()}>
-                    <Text style = {styles.button_text_google}> Google </Text>
-                </TouchableOpacity>
+                <Text style = {styles.Text}>More login options :</Text>
+                <View style={styles.button_container}>
+                    <TouchableOpacity
+                        style={styles.button_fb}
+                        onPress={() => this.props.fbSignIn()}>
+                        <Text style = {styles.button_text_social}> Facebook </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button_google}
+                        onPress={() => this.props.googleSignin()}>
+                        <Text style = {styles.button_text_social}> Google </Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
